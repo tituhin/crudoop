54b297289a6228822ffe69f20ec0ccc4112c2bef
Tariqul Islam Tuhin <60895596+tituhin@users.noreply.github.com>
2022-02-04 12:40:20 +0600

    deleting views

--- Views/book/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Classes\Database;
-
-include_once "../../Classes/Database.php";
-require_once "../../Views/layout/header.php";
-require_once "../../Views/layout/sidebar.php";
-require_once "../../Views/layout/footer.php";
